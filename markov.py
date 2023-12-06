c05493d111be378a84658b85c14cbffa902fca4c
Lucca Wang <luccawang@gmail.com>
2023-12-05 17:08:28 -0700

    Second function completed.

--- markov.py
+++ markov.py
@@ -10,39 +10,60 @@ def open_and_read_file(file_path):
         read_data = f.read()
         return read_data
 
-open_and_read_file("green-eggs.txt")
+def make_chains(text_string):
+    """Take input text as string; return dictionary of Markov chains.
 
+    A chain will be a key that consists of a tuple of (word1, word2)
+    and the value would be a list of the word(s) that follow those two
+    words in the input text.
 
-# def make_chains(text_string):
-#     """Take input text as string; return dictionary of Markov chains.
+    For example:
 
-#     A chain will be a key that consists of a tuple of (word1, word2)
-#     and the value would be a list of the word(s) that follow those two
-#     words in the input text.
+        >>> chains = make_chains('hi there mary hi there juanita')
 
-#     For example:
+    Each bigram (except the last) will be a key in chains:
 
-#         >>> chains = make_chains('hi there mary hi there juanita')
+        >>> sorted(chains.keys()) 
+        [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
 
-#     Each bigram (except the last) will be a key in chains:
+    Each item in chains is a list of all possible following words:
 
-#         >>> sorted(chains.keys()) 
-#         [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
+        >>> chains[('hi', 'there')]
+        ['mary', 'juanita']
 
-#     Each item in chains is a list of all possible following words:
+        >>> chains[('there','juanita')]
+        [None]
+    """
+    
+    #creating a dictionary
+    # #cut the string into a list of strings
+    # words = text.split()
+    # #iterate through the list 
+    #     #for idx, num in range(len(words)):
+    # for i in range(len(words) - 2)
+    # #potential key is first word, second word, in a tuple 
+    # tup_key = (words[i], words[i + 1])
+    #value is a the third word, which we want to put into a list 
+    # value is a list
+    #if the key is already in the dictionary
+        #add the value to the list stored at that key in the dictionary 
+    #if the key is not in the dictionary yet
+        #add the key to the dictionary, have an empty list as the value 
+        #add the string value to that list 
+    #return the dictionary 
+    chains = {}
 
-#         >>> chains[('hi', 'there')]
-#         ['mary', 'juanita']
-
-#         >>> chains[('there','juanita')]
-#         [None]
-#     """
-
-#     chains = {}
-
-#     # your code goes here
-
-#     return chains
+    text = open_and_read_file("green-eggs.txt")
+    text_string = text.split()
+    
+    for i in range(len(text_string)-2):
+        tup_key = (text_string[i], text_string[i+1])
+        str_val = [text_string[i+2]]
+        #if tup_key not in chains:
+        chains[tup_key] = str_val
+    #print(chains.keys())
+    
+    return chains
 
 
 # def make_text(chains):
@@ -50,7 +71,7 @@ open_and_read_file("green-eggs.txt")
 
 #     words = []
 
-#     # your code goes here
+    
 
 #     return ' '.join(words)
 
